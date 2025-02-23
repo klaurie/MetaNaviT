49d0930cd259c6af5880e45ea00aba5e52407c5f
Kaitlyn Laurie <k.laurie124@gmail.com>
2025-02-23 12:26:50 -0800

    added script to run different entry points

--- scripts/run.sh
+++ scripts/run.sh
@@ -17,12 +17,8 @@ case $1 in
   "build")
     python run.py build
     ;;
-  "test-fs")
-    # Run file system organization tests
-    python -m tests.benchmark_tests.file_system_tests.test_organization
-    ;;
   *)
-    echo "Usage: ./run.sh [generate|dev|setup|build|test-fs]"
+    echo "Usage: ./run.sh [generate|dev|setup|build]"
     exit 1
     ;;
 esac
